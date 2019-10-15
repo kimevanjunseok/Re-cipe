7cc222414a8d6be6799c527ca06d5a14dd7b4d81
JO SEONG-WON <gogumassafy@gmail.com>
2019-10-15 11:40:24 +0900

    indentation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number,
+  age: Number
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,30 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var User = require('../models/user')
+var User = require("../models/user");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  User.find(function(error, users){
-    if(error){
-        console.log(error);
-    } else{
-        console.log(users);
-        res.send(users)
+router.get("/", function(req, res, next) {
+  User.find(function(error, users) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(users);
+      res.send(users);
     }
-  })
+  });
 });
 
-router.post('/create', function(req, res, next) {
-  var newUser = new User({name:'Hong Gil Dong', age:'22'});
+router.post("/create", function(req, res, next) {
+  var newUser = new User({ name: "Hong Gil Dong", age: "22" });
 
   User.create(newUser)
-  .then(result => {
-    res.send(result)
-  })
-  .catch(err => {
-    res.status(500).send(err)
-  })
-})
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 module.exports = router;
