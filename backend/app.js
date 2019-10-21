5b3671ce805b7e79baae25b675250d2e822dd6d8
kimevanjunseok <jnunseok@gmail.com>
2019-10-21 16:20:09 +0900

    코드 정리

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,6 @@ var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-//login session
-var session = require('express-session');
-
 var app = express();
 
 // view engine setup
@@ -25,18 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-// login
-app.use(cookieParser())
-app.use(session({
-  key: 'sid',
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 24000 * 60 * 60 // 쿠키 유효기간 24시간
-  }
-}));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
