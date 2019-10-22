1f65de24669bfef387c2afea498541586f738a82
JO SEONG-WON <gogumassafy@gmail.com>
2019-10-22 10:02:40 +0900

    | SW | Ingredinet R

--- backend/routes/ingredient.js
+++ backend/routes/ingredient.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
-var Recipe = require("../models/ingredient");
+var Ingredient = require("../models/ingredient");
 
-router.get("/detail/:recipe_id", function(req, res) {
-  Recipe.findOne({ _id: req.params.recipe_id }, function(err, recipe) {
+router.get("/detail/:ingredient_id", function(req, res) {
+  Ingredient.findOne({ _id: req.params.ingredient_id }, function(
+    err,
+    ingredient
+  ) {
     if (err) return res.status(500).json({ error: err });
-    if (!recipe) return res.status(404).json({ error: "recipe not found" });
-    res.jsoon(recipe);
+    if (!ingredient)
+      return res.status(404).json({ error: "ingredient not found" });
+    res.jsoon(ingredient);
   });
 });
 
